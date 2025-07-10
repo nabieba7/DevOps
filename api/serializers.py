c57d1b423618981d135ad9c608bb72f70ee48b75
nabieba1 <nabieba7@gmail.com>
2025-07-10 11:22:28 +0100

    fixed error

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Book
 
-class BookSerializer(serializer.Serializer):
-    class Mets:
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
         model = Book
         fields = ['title', 'description', 'author', 'created_at']
