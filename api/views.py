4a471c0f2152333660a07606c42cbcec8a0b70eb
nabieba1 <nabieba7@gmail.com>
2025-07-14 13:27:18 +0100

    feat: added new enpoint for book creation

--- api/views.py
+++ api/views.py
@@ -18,10 +18,20 @@ health_view = HealthView.as_view()
 # Create your views here.
 
 class BookView(APIView):
+    """  List all books , or creat a new book            """
     def get(self, request, *args, **kwargs):
         all_books = Book.objects.all()
         serializer = BookSerializer(all_books, many=True)
         return Response(serializer.data)
+    
+    
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 
 # Use this in your urls.py
 book_view = BookView.as_view()
